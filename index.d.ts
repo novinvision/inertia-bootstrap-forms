d363641fcce5a2a973b8ed93ff7b4dcf31b91e6d
Mehran Ranji <mehran.ranji@gmail.com>
2025-08-25 10:12:12 +0330

    index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module "vue3-bootstrap-components" {
+    import { DefineComponent } from "vue";
+
+    // هر کامپوننت به این شکل تعریف میشه
+    export const AmountInput: DefineComponent<{}, {}, any>;
+    export const CaptchaInput: DefineComponent<{}, {}, any>;
+    export const CheckboxButtonInput: DefineComponent<{}, {}, any>;
+    export const CheckboxInput: DefineComponent<{}, {}, any>;
+    export const CheckboxToggle: DefineComponent<{}, {}, any>;
+    export const EditorInput: DefineComponent<{}, {}, any>;
+    export const EmailInput: DefineComponent<{}, {}, any>;
+    export const FileInput: DefineComponent<{}, {}, any>;
+    export const FileUploader: DefineComponent<{}, {}, any>;
+    export const FormContainer: DefineComponent<{}, {}, any>;
+    export const FormLabel: DefineComponent<{}, {}, any>;
+    export const GroupControl: DefineComponent<{}, {}, any>;
+    export const LocationInput: DefineComponent<{}, {}, any>;
+    export const MobileInput: DefineComponent<{}, {}, any>;
+    export const MultiQuantityInput: DefineComponent<{}, {}, any>;
+    export const PasswordInput: DefineComponent<{}, {}, any>;
+    export const PersianDatePickerInput: DefineComponent<{}, {}, any>;
+    export const QuantityInput: DefineComponent<{}, {}, any>;
+    export const SecondarySubmitButton: DefineComponent<{}, {}, any>;
+    export const Select2Input: DefineComponent<{}, {}, any>;
+    export const StarRatingInput: DefineComponent<{}, {}, any>;
+    export const SubmitButton: DefineComponent<{}, {}, any>;
+    export const TelInput: DefineComponent<{}, {}, any>;
+    export const TextAreaInput: DefineComponent<{}, {}, any>;
+    export const TextInput: DefineComponent<{}, {}, any>;
+}
