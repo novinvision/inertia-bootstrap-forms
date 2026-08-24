5bb69d7e9e2fa1a25c54cb5e7363ce72a5839e33
Mehran Ranji <mehran.ranji@gmail.com>
2026-08-24 18:40:22 +0330

    amount input debug

--- src/AmountInput.vue
+++ src/AmountInput.vue
@@ -1,63 +1,76 @@
 <script>
 import {InputGroup, InputGroupText} from 'vue3-bootstrap-components';
-import {defineComponent, inject} from "vue";
+import {computed, defineComponent, inject} from "vue";
 
 export default defineComponent({
-    components: {InputGroup, InputGroupText},
-    props: {
-        name: {
-            type: String,
-            required: true,
-        },
-        required: Boolean,
-        disabled: Boolean,
-        readonly: Boolean,
-        placeholder: {
-            type: String,
-            default: 'عدد را وارد کنید'
-        },
-        unit: {
-            type: String,
-            default: 'عدد'
-        },
+  components: {InputGroup, InputGroupText},
+  props: {
+    name: {
+      type: String,
+      required: true,
     },
-    setup(props) {
-        let form = inject('form');
-
-        if (form === undefined) {
-            form = {
-                errors: {},
-                getID(name) {
-                }
-            };
-        }
-
-        return {form};
+    required: Boolean,
+    disabled: Boolean,
+    readonly: Boolean,
+    placeholder: {
+      type: String,
+      default: 'عدد را وارد کنید'
     },
-    methods: {
-        onChange(event) {
-            this.form[this.name] = parseFloat(this.$number.toEnglish(event.detail?.unmasked));
-            this.$emit('update:modelValue', parseFloat(this.$number.toEnglish(event.detail?.unmasked)));
+    unit: {
+      type: String,
+      default: 'عدد'
+    },
+  },
+  setup(props) {
+    let form = inject('form', {
+      errors: {},
+      getID(name) {
+        return name;
+      }
+    });
+    let group = inject('group', {});
+
+    const modelValue = computed({
+      get() {
+        return (group.value && form.value[group.value.name]) ? group.value?.getData(props.name) : form.value[props.name];
+      },
+      set(value) {
+        if (group?.value?.name) {
+          group.value.setData(props.name, value);
+        } else {
+          form.value[props.name] = value;
         }
-    }
+      }
+    });
+
+    const displayValue = computed({
+      get() {
+        const raw = modelValue.value;
+        if (raw === null || raw === undefined || raw === '') return '';
+        return Number(raw).toLocaleString('en-US');
+      },
+      set(value) {
+        const digitsOnly = String(value).replace(/\D/g, '');
+        modelValue.value = digitsOnly === '' ? null : parseInt(digitsOnly, 10);
+      }
+    });
+
+    return {modelValue, displayValue, form, group};
+  },
 })
 </script>
 <template>
-    <InputGroup>
-        <input
-            :name="name"
-            :value="form[name]"
-            v-maska
-            data-maska="###,###,###,###,###,###,###"
-            data-maska-reversed
-            @maska="onChange"
-            :class="{'is-invalid': form.errors[name]}"
-            :disabled="disabled || form.processing"
-            :readonly="readonly"
-            :placeholder="placeholder"
-            type="tel"
-            class="form-control fanum text-start">
-        <InputGroupText class="fanum">{{ unit }}</InputGroupText>
-        <slot name="suffix"/>
-    </InputGroup>
+  <InputGroup>
+    <input
+        :name="name"
+        v-model="displayValue"
+        :class="{'is-invalid': form.errors[name]}"
+        :disabled="disabled || form.processing"
+        :readonly="readonly"
+        :placeholder="placeholder"
+        type="tel"
+        class="form-control fanum text-start">
+    <InputGroupText class="fanum">{{ unit }}</InputGroupText>
+    <slot name="suffix"/>
+  </InputGroup>
 </template>
